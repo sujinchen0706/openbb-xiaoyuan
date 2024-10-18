eb2547f60b96366774605d859532ca90efc4e1b8
苏晋晨 <2548272059@qq.com>
2024-10-18 18:06:28 +0800

    企业生命周期

---
+++ openbb_xiaoyuan/standard_models/enterprise_life_cycle.py
@@ -0,0 +1,44 @@
+"""企业生命周期"""
+import datetime
+from datetime import date as dateType
+from typing import Optional, Union
+
+from openbb_core.provider.abstract.data import Data
+from openbb_core.provider.abstract.query_params import QueryParams
+from openbb_core.provider.utils.descriptions import (
+    DATA_DESCRIPTIONS,
+    QUERY_DESCRIPTIONS,
+)
+from pydantic import Field, field_validator
+
+class EnterpriseLifeCycleQueryParams(QueryParams):
+    """企业生命周期查询参数"""
+    symbol: str = Field(description=QUERY_DESCRIPTIONS.get("symbol", ""))
+    start_date: Optional[dateType] = Field(
+        default=None,
+        description=QUERY_DESCRIPTIONS.get("start_date", ""),
+    )
+    end_date: Optional[dateType] = Field(
+        default=None,
+        description=QUERY_DESCRIPTIONS.get("end_date", ""),
+    )
+
+    @field_validator("symbol", mode="before", check_fields=False)
+    @classmethod
+    def to_upper(cls, v: str) -> str:
+        """Convert field to uppercase."""
+        return v.upper()
+class EnterpriseLifeCycleData(Data):
+    """企业生命周期数据"""
+    symbol: str = Field(
+        description=DATA_DESCRIPTIONS.get("symbol", "The symbol of the company.")
+    )
+    timestamp: str = Field(
+        description=DATA_DESCRIPTIONS.get("timestamp", "日期")
+    )
+    企业生命周期: float = Field(
+        description=DATA_DESCRIPTIONS.get("企业生命周期", "企业生命周期")
+    )
+
+
+
